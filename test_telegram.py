aca0e4f7efe5548b20641b19214d189dee0e22b7
diegocastroms-svg <diegocastroms@gmail.com>
2025-10-22 18:01:41 -0300

    Delete test_telegram.py

--- test_telegram.py
+++
@@ -1,9 +0,0 @@
-import requests, os
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-msg = "🔔 Teste de mensagem – estou funcionando!"
-
-r = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-                  data={"chat_id": CHAT_ID, "text": msg})
-print(r.status_code, r.text)

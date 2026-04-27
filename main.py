ee5e697e0e3771da4c601346047dd0be172c4eb8
diegocastroms-svg <diegocastroms@gmail.com>
2026-04-27 20:01:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def can_alert(sym):
     key=f"1d_{sym}"
     cd=cooldowns.get(key,0)
     n=time.time()
-    if n-cd>=14400:
+    if n-cd>=86400:
         cooldowns[key]=n
         return True
     return False
@@ -74,7 +74,7 @@ def can_alert_4h(sym):
     key=f"4h_{sym}"
     cd=cooldowns.get(key,0)
     n=time.time()
-    if n-cd>=7200:
+    if n-cd>=86400:
         cooldowns[key]=n
         return True
     return False
